f68f6e6a6fe37e7c72355ee8857087711c6f7e96
Eduardo da Silva <eduardo.socio14@gmail.com>
2022-05-02 17:34:13 -0300

    commit inicial

---
+++ Calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./Somar");
+var SubFunc = require("./sub");
+var MultiFunc = require("./multi");
+var DivFunc = require("./div");
+
+console.log(SomaFunc(2,4));
+console.log(SubFunc(2,4));
+console.log(MultiFunc(2,4));
+console.log(DivFunc(2,4));
